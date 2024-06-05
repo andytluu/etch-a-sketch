bb3a28643068d2e55e493a02f30af27434b1b0bc
Andy Luue <155850018+andytluu@users.noreply.github.com>
2024-06-05 15:42:56 -0700

    Added hover and trail effect

--- eas.js
+++ eas.js
@@ -3,5 +3,23 @@ let parent = document.querySelector("body");
 for(let i = 0; i<256; i++){
     let div = document.createElement("div");
     div.textContent = i.toString();
+    div.classList.toggle("hover");
     parent.appendChild(div);
 }
+
+function changeColor(target){
+    target.style.backgroundColor = "red";
+}
+let hover = document.querySelectorAll(".hover");
+
+hover.forEach(function(elem){
+    elem.addEventListener("mouseenter", function(){
+        elem.style.backgroundColor = "red";
+    })
+})
+
+hover.forEach(function(elem){
+    elem.addEventListener("mouseleave", function(){
+        elem.style.backgroundColor = "blue";
+    })
+})
