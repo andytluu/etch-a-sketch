469402f89600f6f501157e5103f43da448baa9f9
Andy Luue <155850018+andytluu@users.noreply.github.com>
2024-06-07 20:02:42 -0700

    Add event for button

--- eas.js
+++ eas.js
@@ -1,16 +1,37 @@
 let parent = document.querySelector("#container");
 let button = document.querySelector("button");
 
+
 for(let i = 0; i<256; i++){
+
     let div = document.createElement("div");
     div.textContent = i.toString();
-    div.classList.toggle("hover");
+    div.classList.toggle("hover"); 
     parent.appendChild(div);
+    
 }
 
-function changeColor(target){
-    target.style.backgroundColor = "red";
-}
+button.addEventListener("click",function(){
+    let grid = prompt("How many squares?");
+    parent.replaceChildren();
+    for(let i = 0;i < (grid * grid); i++){
+        let div = document.createElement("div");
+        div.textContent = i.toString();
+        div.classList.toggle("hover"); 
+        parent.appendChild(div);
+
+        div.addEventListener("mouseenter", function(){
+            div.style.backgroundColor = "red";
+        });
+
+        div.addEventListener("mouseleave", function(){
+            div.style.backgroundColor = "blue";
+        })
+    }
+
+    
+});
+
 let hover = document.querySelectorAll(".hover");
 
 hover.forEach(function(elem){
@@ -24,12 +45,3 @@ hover.forEach(function(elem){
         elem.style.backgroundColor = "blue";
     })
 })
-
-
-button.addEventListener("click", function(){
-    let grid = prompt("How many squares per side?");
-    if(grid > 101){
-        grid = prompt("Enter a number less than 100?");
-    }
-    console.log(grid);
-});
